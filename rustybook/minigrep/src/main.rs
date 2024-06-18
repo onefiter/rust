b43ba1f84e9d5c5c2554f9f820c84279c997e42a
onefiter <onefiter@gmail.com>
2024-06-18 11:00:46 +0800

    run错误处理

--- rustybook/minigrep/src/main.rs
+++ rustybook/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -14,14 +15,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
